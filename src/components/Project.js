1c435383eb6000333f678241b9dc624aee4c9837
rabin-n14 <74393889+rabin-n14@users.noreply.github.com>
2021-11-12 15:33:58 +1100

    Hover Links to Projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,6 +4,8 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import CodeIcon from "@mui/icons-material/Code";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import Fade from "react-reveal/Fade";
 
 function Project() {
@@ -14,7 +16,7 @@ function Project() {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 4000,
+    autoplaySpeed: 5000,
     pauseOnFocus: true,
   };
 
@@ -30,6 +32,27 @@ function Project() {
               <SlideImgDes>
                 <SlideImage>
                   <img src="/images/tesla-clone.png" alt="Tesla-Clone-Img" />
+                  <FadedBox>
+                    <a
+                      href="https://rabin-tesla-clone-2021.netlify.app/"
+                      target="_blank"
+                    >
+                      <div className="boxSides">
+                        View Project
+                        <VisibilityIcon />
+                      </div>
+                    </a>
+                    <div className="vrLine"></div>
+                    <a
+                      href="https://github.com/rabin-n14/tesla-clone-app"
+                      target="_blank"
+                    >
+                      <div className="boxSides">
+                        Source Code
+                        <GitHubIcon />
+                      </div>
+                    </a>
+                  </FadedBox>
                 </SlideImage>
                 <Fade bottom duration={3000}>
                   <SlideDescription>
@@ -53,6 +76,27 @@ function Project() {
               <SlideImgDes>
                 <SlideImage>
                   <img src="/images/disney-clone.png" alt="Disney-Clone-Img" />
+                  <FadedBox>
+                    <a
+                      href="https://disneyplus-clone-rabin.firebaseapp.com"
+                      target="_blank"
+                    >
+                      <div className="boxSides">
+                        View Project
+                        <VisibilityIcon />
+                      </div>
+                    </a>
+                    <div className="vrLine"></div>
+                    <a
+                      href="https://github.com/rabin-n14/disney-clone"
+                      target="_blank"
+                    >
+                      <div className="boxSides">
+                        Source Code
+                        <GitHubIcon />
+                      </div>
+                    </a>
+                  </FadedBox>
                 </SlideImage>
                 <Fade bottom duration={3000}>
                   <SlideDescription>
@@ -146,23 +190,77 @@ const SlideImgDes = styled.div`
 const SlideImage = styled.div`
   height: 40vh;
   /* background-color: red; */
-  padding: 3px;
+  position: relative;
+  display: inline-block;
+
   img {
     width: 100%;
     height: 100%;
     object-fit: contain;
     opacity: 1;
-    box-shadow: 0px 5px 21px 1px rgba(119, 119, 119, 0.88);
     transition-duration: 0.5s;
-    &:hover {
-      border-radius: 8px;
-      box-shadow: 1px 10px 21px 5px rgba(119, 119, 119, 0.88);
-
-      transform: scale(1.03);
-      transition-duration: 0.5s;
-    }
   }
 `;
+
+const FadedBox = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.6);
+  opacity: 0; /*change*/
+  transition-duration: 0.5s;
+  &:hover {
+    opacity: 1;
+    transition-duration: 0.5s;
+    box-shadow: 0px 5px 21px 1px rgba(119, 119, 119, 0.88);
+    .vrLine {
+      opacity: 1;
+      transition-duration: 0.5s;
+      transform: translateY(0);
+    }
+    .boxSides {
+      opacity: 1;
+      transition-duration: 0.5s;
+      transform: translateY(0);
+    }
+  }
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  .vrLine {
+    border: 1px solid #fff;
+    height: 95%;
+    position: absolute;
+    left: 50%;
+    margin-left: 4px;
+    top: 5px;
+    transform: translateY(-60px);
+    opacity: 0.2;
+    transition: 0.25s;
+  }
+  .boxSides {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    transform: translateY(80px);
+    opacity: 0.2;
+    transition-duration: 0.25s;
+    font-family: "Shalimar", cursive;
+    font-size: 30px;
+    &:hover {
+      transition-duration: 0.25s;
+      font-size: 40px;
+      cursor: pointer;
+    }
+  }
+  a:visited {
+    color: #fff;
+  }
+`;
+
 const SlideDescription = styled.div`
   /* background-color: orange; */
   font-family: "Open Sans Condensed", sans-serif;
