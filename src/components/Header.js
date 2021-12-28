6491fbcab83d64c227e04528af7c74fc27e3a827
rabin-n14 <74393889+rabin-n14@users.noreply.github.com>
2021-12-28 17:43:51 +1100

    Decent Nav Menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,33 +13,33 @@ function Header() {
           <li className="menuList">
             <a href="#">
               <span className="icon">
-                <HomeOutlinedIcon />
+                <HomeOutlinedIcon fontSize="large" />
               </span>
-              <span className="text">Home</span>
+              <span className="text">HOME</span>
             </a>
           </li>
           <li className="menuList">
             <a href="#">
               <span className="icon">
-                <IntegrationInstructionsOutlinedIcon />
+                <IntegrationInstructionsOutlinedIcon fontSize="large" />
               </span>
-              <span className="text">Projects</span>
+              <span className="text">PROJECTS</span>
             </a>
           </li>
           <li className="menuList">
             <a href="#">
               <span className="icon">
-                <PsychologyOutlinedIcon />
+                <PsychologyOutlinedIcon fontSize="large" />
               </span>
-              <span className="text">Experience</span>
+              <span className="text">EXPERIENCE</span>
             </a>
           </li>
           <li className="menuList">
             <a href="#">
               <span className="icon">
-                <CallOutlinedIcon />
+                <CallOutlinedIcon fontSize="large" />
               </span>
-              <span className="text">Contact</span>
+              <span className="text">CONTACT</span>
             </a>
           </li>
         </NavMenu>
@@ -51,7 +51,7 @@ function Header() {
 export default Header;
 
 const Nav = styled.div`
-  min-height: 10vh;
+  min-height: 8vh;
   /* background-color: black; */
   display: flex;
   justify-content: center;
@@ -62,20 +62,52 @@ const NavMenu = styled.div`
   display: flex;
   flex-direction: row;
   width: 60%;
-  justify-content: space-evenly;
+  justify-content: space-around;
   position: relative;
-
+  /* border: 2px solid rgba(0, 0, 0, 0.5); */
+  border-radius: 10px;
+  /* background-color: green; */
+  /* overflow-y: hidden; */
   li {
     list-style: none;
     display: flex;
     position: relative;
     z-index: 1;
+    :hover a .icon {
+      transform: translateY(-35px);
+    }
+    :hover a .text {
+      transform: translateY(10px);
+      opacity: 1;
+    }
     a {
       position: relative;
       display: flex;
       justify-content: center;
       align-items: center;
       color: white;
+      flex-direction: column;
+      text-align: center;
+
+      .icon {
+        position: relative;
+        display: block;
+        font-size: 50px;
+
+        line-height: 75px;
+        text-align: center;
+        transition: 0.5s;
+      }
+
+      .text {
+        position: absolute;
+        font-family: "Moon Dance", cursive;
+        font-weight: bold;
+        font-size: 18px;
+        letter-spacing: 2px;
+        transition: 0.5s;
+        opacity: 0;
+      }
     }
     a:link {
       text-decoration: none;
